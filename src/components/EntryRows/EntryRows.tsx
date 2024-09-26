745e32dc0ad23228ee8c06aaa8574ae29bfe3763
Jong Kim <kim.jongjhun@gmail.com>
2024-09-26 12:51:23 -0400

    Initial layout.

--- src/components/EntryRows/EntryRows.tsx
+++ src/components/EntryRows/EntryRows.tsx
@@ -1,13 +1,43 @@
-const styles = {
-  background: "green",
-  padding: "8px",
+import Grid from "@mui/material/Grid2";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableFooter from "@mui/material/TableFooter";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+
+const tableHeaders = ["Date", "Client", "Hours", "Location", "Actions"];
+
+const RenderTableHeaders = () => {
+  return tableHeaders.map((header) => {
+    return <TableCell>{header}</TableCell>;
+  });
 };
 
 const EntryRows = () => {
   return (
-    <div style={styles}>
-      <div>entry rows</div>
-    </div>
+    <Grid container spacing={1}>
+      <Grid size={{ xs: 12, md: 8 }} offset={{ xs: 0, md: 2 }}>
+        <TableContainer component={Paper}>
+          <Table size={"small"}>
+            <TableHead>
+              <TableRow>
+                <RenderTableHeaders />
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow></TableRow>
+            </TableBody>
+            <TableFooter>
+              <TableRow></TableRow>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
   );
 };
 
